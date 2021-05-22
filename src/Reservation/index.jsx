28f18210b12c083716d4c8971d233f867be855b9
Zuzana Franova <franova.z@gmail.com>
2021-05-22 10:51:51 +0200

    Added routes Home and reservation, style fix

---
+++ src/Reservation/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './style.css';
+
+const Reservation = () => {
+  return (
+    <div>
+      <h2>Reservation</h2>
+    </div>
+  );
+};
+
+export default Reservation;
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,38 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
+import Home from './Home';
+import Reservation from './Reservation';
 import './style.css';
 
 const App = () => (
-  <>
-    <Header />
-    <Footer />
-  </>
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/reservation">Reservation</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/reservation">
+          <Header />
+          <Reservation />
+          <Footer />
+        </Route>
+        <Route path="/">
+          <Header />
+          <Home />
+          <Footer />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 render(<App />, document.querySelector('#app'));
